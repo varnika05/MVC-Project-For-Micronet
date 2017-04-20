b2baed555e848bc1f2cd367843d8ec81c95afa73
varnika05 <varnika05@live.com>
2017-04-20 12:57:01 +0530

    Update UnitTest1.cs

--- Micronet_User/Micronet_User.Tests/UnitTest1.cs
+++ Micronet_User/Micronet_User.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Micronet_User.Controllers;
+using System.Web.Mvc;
 
 namespace Micronet_User.Tests
 {
@@ -7,8 +9,23 @@ namespace Micronet_User.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+       public void TestMethod1()
         {
+            var controller =  new UserController() ;
+            
+
+            ViewResult r = controller.Index() as ViewResult;
+            Assert.IsNotNull(r);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var controller = new UserController();
+            //var result = controller.UserProfile() as ViewResult;
+
+            ViewResult r = controller.UserProfile() as ViewResult;
+            Assert.IsNotNull(r);
         }
     }
 }
